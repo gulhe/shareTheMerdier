5ec8f4b242998cc5d54894c80845c70a4c119423
Guillaume Lhermitte <guillaume.lhermitte@shodo.io>
2021-06-28 10:31:24 +0200

    [Init] Scaffolding du projet
     - les trucs basiques
     - et du 🥒

---
+++ README.md
@@ -0,0 +1,7 @@
+# YAM'S !!
+
+## Description générale
+
+Le Yam's est un est jeu qui se joue avec 5 dés.
+Le résultat de ces 5 dés permet d'obtenir des combinaisons.
+Chaque combinaison vaut un nombre de point.
---
+++ src/test/java/org/fnac/yams/Stepdefs.java
@@ -0,0 +1,12 @@
+package org.fnac.yams;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class Stepdefs {
+
+    /* yet to come */
+
+}
